1e2726aaf33db0f4ddc4096ff91bc82d2cdb8729
Leonardo Zanette <leonardobzanette@gmail.com>
2022-07-18 18:45:26 -0300

    Requisito 2 - DONE

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('Testa o component About', () => {
+  test('testa se o link de navegação HOME esta no documento e funciona', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about');
+
+    const about = screen.getByRole('heading', { name: /About Pokédex/i });
+    expect(about).toBeInTheDocument();
+
+    const img = screen.getByAltText('Pokédex');
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
