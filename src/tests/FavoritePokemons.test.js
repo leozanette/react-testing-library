ad3f90103fa3853b03a353c9612ec1f3d208132c
Leonardo Zanette <leonardobzanette@gmail.com>
2022-07-19 09:55:29 -0300

    Requisito 3 - DONE

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('Testa o component Favorite Pokemons', () => {
+  test('testa se o componente mostra a frase no favorite pokemon found', () => {
+    renderWithRouter(<App />);
+
+    const favoritePokemons = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favoritePokemons).toBeInTheDocument();
+
+    userEvent.click(favoritePokemons);
+    const none = screen.getByText(/no favorite pokemon found/i);
+    expect(none).toBeInTheDocument();
+  });
+});
